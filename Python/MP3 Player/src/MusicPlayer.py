63214c0dc9c2d65884b6576f74811d464ffafd5d
Nick Allan <nickallan.only@gmail.com>
2022-03-06 22:34:21 +0000

    Added MusicPlayer.previous() method

--- Python/MP3 Player/src/MusicPlayer.py
+++ Python/MP3 Player/src/MusicPlayer.py
@@ -9,29 +9,49 @@ class MusicPlayer:
     current_song: Song
 
     def __init__(self, library: MusicLibrary):
-        self.player = None
-        self.library = library
+        self._media_player = None
+        self._library = library
         self.play_list = []
+        self._next_song_index = None
         self.current_song = None
         self.play_list_length = 10
 
     def start(self):
-        self.current_song = self.get_next_song()
-        self.player = vlc.MediaPlayer(self.current_song.full_path)
-        self.player.play()
-
-    def get_next_song(self):
         if not self.play_list:
-            self.play_list = self.library.get_random_playlist(self.play_list_length)
-
-        return self.play_list.pop()
-
-    def next(self):
-        self.stop()
-        self.start()
-
-    def previous(self):
-        self.stop()
+            self.play_list = self._library.get_random_playlist(self.play_list_length)
+        self.next()
 
     def stop(self):
-        self.player.stop()
+        self._media_player.stop()
+
+    def next(self):
+        self.current_song = self._get_next_song()
+        self._media_player = vlc.MediaPlayer(self.current_song.full_path)
+        self._media_player.play()
+
+    def previous(self):
+        self.current_song = self._get_previous_song()
+        self._media_player = vlc.MediaPlayer(self.current_song.full_path)
+        self._media_player.play()
+
+    def _get_next_song(self):
+        if self._next_song_index is None:
+            self._next_song_index = 0
+        elif self._next_song_index == len(self.play_list) - 1:
+            self._next_song_index = None
+            return None
+        else:
+            self._next_song_index += 1
+
+        next_song = self.play_list[self._next_song_index]
+        return next_song
+
+    def _get_previous_song(self):
+        if self._next_song_index == 0:
+            self._next_song_index = None
+            return None
+
+        self._next_song_index -= 1
+        print(self._next_song_index)
+        previous_song = self.play_list[self._next_song_index]
+        return previous_song
--- Python/MP3 Player/tests/test_MusicPlayer.py
+++ Python/MP3 Player/tests/test_MusicPlayer.py
@@ -1,12 +1,41 @@
 from src.MusicLibrary import MusicLibrary
 from src.MusicPlayer import MusicPlayer
 
+first_song = "1st"
+second_song = "2nd"
+third_song = "3rd"
+
 
 def create_sut():
     library = MusicLibrary("\\\\nicksnas\\Public\\Shared Music\\")
-    return MusicPlayer(library)
+
+    sut = MusicPlayer(library)
+    sut.play_list = [first_song, second_song, third_song]
+
+    return sut
 
 
 def test_get_next_song():
     sut = create_sut()
-    assert sut.get_next_song()
+
+    assert sut._get_next_song() == first_song
+    assert sut._get_next_song() == second_song
+    assert sut._get_next_song() == third_song
+    assert sut._get_next_song() is None
+
+
+def test_get_previous_song():
+    sut = create_sut()
+    assert sut._get_next_song() == first_song
+    assert sut._get_next_song() == second_song
+    assert sut._get_previous_song() == first_song
+    assert sut._get_previous_song() is None
+
+
+def test_next_then_previous_song():
+    sut = create_sut()
+    assert sut._get_next_song() == first_song
+    assert sut._get_next_song() == second_song
+    assert sut._get_next_song() == third_song
+    assert sut._get_previous_song() == second_song
+    assert sut._get_previous_song() == first_song
