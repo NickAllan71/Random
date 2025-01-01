acacccb296f6b418ef0519992a1c2b752995d451
Nick Allan <nickallan.only@gmail.com>
2025-01-01 12:34:16 +0000

    Refactor import statements and reload main module in template_stub.py

--- Python/BlenderViaPyCharm/src/template_stub.py
+++ Python/BlenderViaPyCharm/src/template_stub.py
@@ -1,12 +1,12 @@
 import sys
 import os
 import bpy
+import main
+import importlib
 
 blend_dir = os.path.dirname(bpy.data.filepath)
 sys.path.append(blend_dir)
 
-import main
-import importlib
 
 importlib.reload(main)
 main.main()
