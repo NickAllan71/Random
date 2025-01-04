cc582e3896a2b96b68e4be6d41f71661af85f8a9
Nick Allan <nickallan.only@gmail.com>
2025-01-04 23:09:16 +0000

    Remove unimportant_words.txt and update file path handling in main.py; enhance user input defaults and key press handling for repeated analysis.

--- Python/ResumeHelper/python-workspace/src/main.py
+++ Python/ResumeHelper/python-workspace/src/main.py
@@ -3,13 +3,14 @@ from file_handler import FileHandler
 from keyword_visualiser import KeywordVisualiser
 from settings import Settings
 import msvcrt
+import os
 
 GREEN = '\033[92m'
 RESET = '\033[0m'
 ESC_KEY = b'\x1b'
 
-def get_folder_input(prompt, settings, key):
-    default = settings.get(key)
+def get_folder_input(prompt, settings, key, default_value=None):
+    default = settings.get(key, default_value)
     value = None
     while not value:
         value = input(f"{prompt} [{GREEN}{default or ''}{RESET}]: ").strip()
@@ -34,25 +35,37 @@ def get_paths():
         args.job_description_folder = get_folder_input(
             "Job description folder",
             settings,
-            'job_description_folder'
+            'job_description_folder',
+            default_value='.'
         )
     
     if not args.resume_folder:
         args.resume_folder = get_folder_input(
             "Resume folder",
             settings,
-            'resume_folder'
+            'resume_folder',
+            default_value='.'
         )
     
     if not args.unimportant_words_file_path:
         args.unimportant_words_file_path = get_folder_input(
-        "Unimportant words file",
-        settings,
-        'unimportant_words_file_path'
-    )
+            "Unimportant words file",
+            settings,
+            'unimportant_words_file_path',
+            r"C:\Users\Nick\GitHub\Random\Python\ResumeHelper\input_files\unimportant_words.txt"
+        )
 
     return args.job_description_folder, args.resume_folder, args.unimportant_words_file_path
 
+def handle_key_press():
+    print("Press ESC to quit, or Enter to repeat.")
+    while True:
+        key = msvcrt.getch()
+        if key == ESC_KEY:
+            return False
+        elif key == b'\r':  # b'\r' is the Enter key
+            return True
+    
 if __name__ == "__main__":
     job_description_folder, resume_folder, unimportant_words_file_path = get_paths()
     file_handler = FileHandler(
@@ -77,8 +90,4 @@ if __name__ == "__main__":
         keyword_visualiser.print_legend()
         keyword_visualiser.visualise()
 
-        print("Press ESC to quit, or any other key to repeat.")
-        if msvcrt.getch() == ESC_KEY:
-            repeating = False
-        else:
-            repeating = True
+        repeating = handle_key_press()
