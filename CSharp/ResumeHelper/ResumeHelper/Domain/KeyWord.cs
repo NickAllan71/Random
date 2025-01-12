968ca9e275b57772e435cfc3b313e8483255f3bb
Nick Allan <nickallan.only@gmail.com>
2025-01-12 22:04:39 +0000

    Enhance KeyWord class with color properties for console output based on keyword importance

--- CSharp/ResumeHelper/ResumeHelper/Domain/KeyWord.cs
+++ CSharp/ResumeHelper/ResumeHelper/Domain/KeyWord.cs
@@ -8,6 +8,17 @@ public class KeyWord(
     public readonly string Word = word;
     public KeywordImportance Importance { get; internal set; } = importance;
     public bool IsMatch { get; internal set; } = isMatch;
+    
+    public ConsoleColor ForegroundColor => IsMatch ? ConsoleColor.White : ImportanceColor;
+    public ConsoleColor BackgroundColor => IsMatch ? ImportanceColor : ConsoleColor.Black;
+
+    private ConsoleColor ImportanceColor => Importance switch
+    {
+        KeywordImportance.High => ConsoleColor.Red,
+        KeywordImportance.Medium => ConsoleColor.Green,
+        KeywordImportance.Low => ConsoleColor.DarkGray,
+        _ => ConsoleColor.DarkGray
+    };
 
     public override string ToString()
     {
